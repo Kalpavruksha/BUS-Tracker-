9fa488751ae3595a67681e46869906441bc6a959
kalpavruksha G <154514800+Kalpavruksha@users.noreply.github.com>
2025-09-22 15:12:52 +0530

    Update buses.js

--- server/routes/buses.js
+++ server/routes/buses.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Bus = require('../models/Bus');
 
-
-
 // Get all active buses
 router.get('/', async (req, res) => {
   try {
